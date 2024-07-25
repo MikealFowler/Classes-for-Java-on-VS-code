20adf3d296873077dda0d6f36b5983c72b891849
Mikeal Fowler <mikeyfowler36@gmail.com>
2024-07-25 11:38:16 -0300

    Testing

--- Java/Main.java
+++ Java/Main.java
@@ -1,10 +1,10 @@
 
-
-
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("");
+ public static void main(String[] args) {
+        // System.out.println(33/4);
+        // System.out.println((33.0/4) % 7);
+        // System.out.println(8 % 7.0);
+        System.out.println((33 / 4) % 7);
 
      
     }

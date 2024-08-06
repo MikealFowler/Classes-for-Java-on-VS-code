9c512fa58d65a01208dd4c14e4fb57a91aea3a6e
Mikeal Fowler <mikeyfowler36@gmail.com>
2024-08-06 19:34:58 -0300

    I added my html files and css for my website to go on amazon

--- Java/Main.java
+++ Java/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
  public static void main(String[] args) {
-        // System.out.println(33/4);
-        // System.out.println((33.0/4) % 7);
-        // System.out.println(8 % 7.0);
+        System.out.println(33/4);
+        System.out.println((33.0/4) % 7);
+        System.out.println(8 % 7.0);
         System.out.println((33 / 4) % 7);
 
      
